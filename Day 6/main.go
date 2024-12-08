9182818ee7ed9e5439352f86bbe6e0a4d0b5c92f
Jan <janbar2001@gmail.com>
2024-12-08 13:31:52 +0100

    Day 6 Part 1 and partiall Part 2

--- Day 6/main.go
+++ Day 6/main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Println("Day 6")
 	fmt.Println("Part 1")
 	part1()
+	fmt.Println("Part 2")
+	part2()
 }
 
 func part1() {
@@ -18,7 +20,7 @@ func part1() {
 	inputString := string(input)
 	inputSlice := strings.Split(inputString, "\r\n")
 
-	var uniqueTiles []int
+	var uniqueTiles []string
 	direction := 0 // 0=up, 1=right, 2=down, 3=left
 	position := []int{0, 0}
 
@@ -41,7 +43,6 @@ func part1() {
 
 	done = false
 	for done == false {
-		fmt.Println(position, direction)
 		// get if the next Cell is valid
 		nextCellValid := true
 		switch direction {
@@ -58,7 +59,7 @@ func part1() {
 			position[1] = position[1] - 1
 			break
 		case 1:
-			if position[0]+1 == len(inputSlice[0])+1 {
+			if position[0]+1 == len(inputSlice[0]) {
 				done = true
 				nextCellValid = false
 				break
@@ -70,7 +71,7 @@ func part1() {
 			position[0] = position[0] + 1
 			break
 		case 2:
-			if position[1]+1 == len(inputSlice)+1 {
+			if position[1]+1 == len(inputSlice) {
 				done = true
 				nextCellValid = false
 				break
@@ -98,33 +99,143 @@ func part1() {
 		}
 
 		if nextCellValid {
-
-			var padding int = 1
-			for padding < position[1] {
-				padding *= 10
-			}
-			num := position[0]*padding + position[1]
+			num := strings.Join([]string{string(position[0]), string(position[1])}, "")
 			if !slices.Contains(uniqueTiles, num) {
 				uniqueTiles = append(uniqueTiles, num)
 			}
 		} else {
-			fmt.Println("TURN AROUND")
 			direction++
 			direction = direction % 4
-			fmt.Println(direction)
 		}
 
 	}
 
-	fmt.Println(direction, position)
+	fmt.Println(len(uniqueTiles) + 1)
+}
 
-	fmt.Println(inputSlice)
-	fmt.Println(uniqueTiles)
-	fmt.Println(len(uniqueTiles))
+func part2() {
+	input := ReadInput()
+	inputString := string(input)
+	inputSlice := strings.Split(inputString, "\r\n")
+
+	loops := 0
+	direction := 0 // 0=up, 1=right, 2=down, 3=left
+	position := []int{0, 0}
+
+	done := false
+	for i, row := range inputSlice {
+		for j, column := range row {
+			if column == '^' {
+				position = []int{j, i}
+				rowRunes := []rune(row)
+				rowRunes[j] = '.'
+				inputSlice[i] = string(rowRunes)
+				done = true
+				break
+			}
+		}
+		if done {
+			break
+		}
+	}
+
+	startingPosition := make([]int, len(position))
+	copy(startingPosition, position)
+	for i, _ := range inputSlice {
+		for j, _ := range inputSlice[0] {
+			if j == startingPosition[0] && i == startingPosition[1] {
+				continue
+			}
+			copy(position, startingPosition)
+
+			var uniqueTiles []string
+			grid := make([]string, len(inputSlice))
+			copy(grid, inputSlice)
+			rowRunes := []rune(grid[i])
+			rowRunes[j] = '#'
+			grid[i] = string(rowRunes)
+
+			done = false
+			for done == false {
+				// get if the next Cell is valid
+				nextCellValid := true
+				switch direction {
+				case 0:
+					if position[1]-1 == -1 {
+						done = true
+						nextCellValid = false
+						break
+					}
+					if grid[position[1]-1][position[0]] == '#' {
+						nextCellValid = false
+						break
+					}
+					position[1] = position[1] - 1
+					break
+				case 1:
+					if position[0]+1 == len(grid[0]) {
+						done = true
+						nextCellValid = false
+						break
+					}
+					if grid[position[1]][position[0]+1] == '#' {
+						nextCellValid = false
+						break
+					}
+					position[0] = position[0] + 1
+					break
+				case 2:
+					if position[1]+1 == len(grid) {
+						done = true
+						nextCellValid = false
+						break
+					}
+					if grid[position[1]+1][position[0]] == '#' {
+						nextCellValid = false
+						break
+					}
+					position[1] = position[1] + 1
+					break
+
+				case 3:
+					if position[0]-1 == -1 {
+						done = true
+						nextCellValid = false
+						break
+					}
+					if grid[position[1]][position[0]-1] == '#' {
+						nextCellValid = false
+						break
+					}
+					position[0] = position[0] - 1
+					break
+
+				}
+
+				if nextCellValid {
+					num := fmt.Sprintf("%d %d %d", direction, position[0], position[1])
+					if !slices.Contains(uniqueTiles, num) {
+						uniqueTiles = append(uniqueTiles, num)
+					} else {
+						done = true
+						loops++
+						break
+					}
+				} else {
+					direction++
+					direction = direction % 4
+				}
+
+			}
+
+		}
+	}
+
+	fmt.Print(loops)
 }
 
 func ReadInput() []byte {
-	file, err := os.ReadFile("sample.txt")
+	file, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
